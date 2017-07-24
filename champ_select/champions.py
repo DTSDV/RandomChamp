2ebdf40c59f0ff96594893acd4f4ec677f46bd4b
DTSDV <DavisTSDV@hotmail.com>
2017-07-24 14:39:17 -0400

    Add files via upload

---
+++ champ_select/champions.py
@@ -0,0 +1,776 @@
+champions = [
+        {
+            'name':'Aatrox',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Ahri',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Akali',
+            'role':['mid', 'top', 'jungle'],
+            'ad-ap':['ap', 'hybrid'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Alistar',
+            'role':['support'],
+            'ad-ap':['ad', 'ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Amumu',
+            'role':['jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Anivia',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Annie',
+            'role':['mid', 'support'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Ashe',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'frejlord'
+        },
+                {
+            'name':'AurelionSol',
+            'role':['mid', 'top', 'support', 'jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name': 'Azir',
+            'role': ['mid'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Bard',
+            'role': ['support'],
+            'ad-ap': ['ad', 'ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Blitzcrank',
+            'role': ['support'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Brand',
+            'role': ['support', 'mid'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name':'Braum',
+            'role':['support'],
+            'ad-ap':['ad'],
+            'faction':'frejlord'
+        },
+        {
+            'name':'Caitlyn',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Caitlyn',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Camille',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Cassiopeia',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'ChoGath',
+            'role':['top', 'jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Corki',
+            'role':['mid','adc'],
+            'ad-ap':['ad','ap','hybrid'],
+            'faction':'bandlecity'
+        },
+        {
+            'name':'Darius',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Diana',
+            'role':['mid','jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name': 'DrMundo',
+            'role': ['support', 'jungle', 'top'],
+            'ad-ap': ['ap', 'ad'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Draven',
+            'role': ['adc'],
+            'ad-ap': ['ad'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Ekko',
+            'role': ['mid', 'jungle'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Elise',
+            'role': ['jungle'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Evelynn',
+            'role': ['jungle'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Ezreal',
+            'role': ['adc', 'mid'],
+            'ad-ap': ['ad', 'ap', 'hybrid'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Fiddlesticks',
+            'role': ['mid', 'jungle'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Fiora',
+            'role': ['top'],
+            'ad-ap': ['ad'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name':'Fizz',
+            'role':['top','mid','jungle'],
+            'ad-ap':['ap','ad'],
+            'faction':'bilgewater'
+        },
+        {
+            'name': 'Galio',
+            'role': ['mid'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name': 'Galio',
+            'role': ['mid'],
+            'ad-ap': ['ap'],
+            'faction': 'temp_fac'
+        },
+        {
+            'name':'Gangplank',
+            'role':['top','mid'],
+            'ad-ap':['ad'],
+            'faction':'bilgewater'
+        },
+        {
+            'name':'Garen',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'demacia'
+        },
+        {
+            'name':'Gnar',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Gragas',
+            'role':['top','mid','jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Graves',
+            'role':['top', 'jungle', 'adc'],
+            'ad-ap':['ad'],
+            'faction':'bilgewater'
+        },
+        {
+            'name':'Hecarim',
+            'role':['jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Heimerdinger',
+            'role':['top', 'mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Illaoi',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Irelia',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Ivernr',
+            'role':['jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Janna',
+            'role':['support'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'JarvanIV',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Jax',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Katarina',
+            'role':['mid'],
+            'ad-ap':['ap','hybrid'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Kayle',
+            'role':['top', 'mid'],
+            'ad-ap':['ap','ad','hybrid'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Kennen',
+            'role':['top', 'mid', 'adc'],
+            'ad-ap':['ap','ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'KhaZix',
+            'role':['jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Kindred',
+            'role':['jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Kled',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'KogMaw',
+            'role':['adc'],
+            'ad-ap':['ad', 'ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'LeBlanc',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Lee Sin',
+            'role':['jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Leona',
+            'role':['support'],
+            'ad-ap':['ap','ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Lissandra',
+            'role':['top', 'mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Lucian',
+            'role':['adc', 'mid'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Lulu',
+            'role':['top','mid','support'],
+            'ad-ap':['ap'],
+            'faction':'bandlecity'
+        },
+        {
+            'name':'Lux',
+            'role':['support', 'mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Malphite',
+            'role':['top', 'jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Malzahar',
+            'role':['support', 'mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Maokai',
+            'role':['top', 'support', 'jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'MasterYi',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'MissFortune',
+            'role':['adc', 'support'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Mordekaiser',
+            'role':['top', 'mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Morgana',
+            'role':['support', 'mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Nami',
+            'role':['support'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Nasus',
+            'role':['top'],
+            'ad-ap':['ap', 'ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Nautilus',
+            'role':['top', 'support', 'jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Nidalee',
+            'role':['jungle'],
+            'ad-ap':['ap', 'ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Nocturne',
+            'role':['jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Nunu',
+            'role':['jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Olaf',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Orianna',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Pantheon',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Poppy',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad','ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Quinn',
+            'role':['top', 'adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Rammus',
+            'role':['jungle'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'RekSai',
+            'role':['jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Renekton',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Rengar',
+            'role':['jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Riven',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Rumble',
+            'role':['top','mid','jungle'],
+            'ad-ap':['ap'],
+            'faction':'bandlecity'
+        },
+        {
+            'name':'Ryze',
+            'role':['top', 'mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Sejuani',
+            'role':['jungle'],
+            'ad-ap':['ap', 'ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Shaco',
+            'role':['top', 'jungle'],
+            'ad-ap':['ap', 'ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Shen',
+            'role':['top'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Shyvana',
+            'role':['jungle'],
+            'ad-ap':['ad','tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Singed',
+            'role':['top'],
+            'ad-ap':['ap', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Sion',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Sivir',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Skarner',
+            'role':['jungle'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Sona',
+            'role':['mid','support'],
+            'ad-ap':['ap'],
+            'faction':'demacia'
+        },
+        {
+            'name':'Soraka',
+            'role':['support'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Swain',
+            'role':['mid','top'],
+            'ad-ap':['ap'],
+            'faction':'demacia'
+        },
+        {
+            'name':'Syndra',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'TahmKench',
+            'role':['support'],
+            'ad-ap':['ad','ap','tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Taliyah',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Talon',
+            'role':['mid'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Taric',
+            'role':['support'],
+            'ad-ap':['ad','ap','tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Teemo',
+            'role':['top'],
+            'ad-ap':['ad','ap','hybrid'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Thresh',
+            'role':['support'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Tristana',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Trundle',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Tryndamere',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'TwistedFate',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Twitch',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Udyr',
+            'role':['jungle'],
+            'ad-ap':['ad','ap','tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Urgot',
+            'role':['top'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Varus',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Vayne',
+            'role':['adc'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Veigar',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'VelKoz',
+            'role':['mid','support'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Vi',
+            'role':['jungle'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Viktor',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Vladmir',
+            'role':['top',' mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Volibear',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Warwick',
+            'role':['jungle'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Wukong',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Xerath',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'XinZhao',
+            'role':['top', 'jungle'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Yasuo',
+            'role':['top', 'mid'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Yorick',
+            'role':['top'],
+            'ad-ap':['ad', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Zac',
+            'role':['jungle'],
+            'ad-ap':['ap', 'tank'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Zed',
+            'role':['top', 'mid'],
+            'ad-ap':['ad'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Ziggs',
+            'role':['mid'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Zileann',
+            'role':['mid', 'support'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+        {
+            'name':'Zyra',
+            'role':['mid', 'support'],
+            'ad-ap':['ap'],
+            'faction':'temp_fac'
+        },
+    ]
